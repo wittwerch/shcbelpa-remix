59e08fe784105be5babe03a4eb763e3af8014d3f
Christian Wittwer <wittwerch@gmail.com>
2023-11-06 08:33:04 +0100

    Error boundry

--- app/routes/game.$id.tsx
+++ app/routes/game.$id.tsx
@@ -1,30 +1,23 @@
 import SingleColumnPage from "~/components/ui/SingleColumnPage";
 import { useLoaderData } from "@remix-run/react";
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { getGame } from "~/lib/api";
 import { Timeline } from "~/components/Timeline";
 import { GameHeader } from "~/components/GameHeader";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import GameRoster from "~/components/GameRoster";
+import React from "react";
 
-// export const meta: MetaFunction<typeof loader> = ({ data }) => {
-//   return [
-//     { title: data?.title || "SHC Belpa 1107" },
-//     { name: "description", content: data?.description || "" },
-//   ];
-// };
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  return [{ title: "SHC Belpa 1107" }];
+};
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.id, "params.id is required");
 
   return await getGame(params.id);
 };
-
-const pages = [
-  { name: "NLA", href: "/1/season/2324", current: false },
-  { name: "Saison 2023/2024", href: "#", current: true },
-];
 
 export default function Game() {
   const data = useLoaderData<typeof loader>();
@@ -56,5 +49,28 @@ export default function Game() {
         </div>
       </SingleColumnPage>
     </>
+  );
+}
+
+export function ErrorBoundary() {
+  return (
+    <SingleColumnPage>
+      <div
+        className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative flex flex-col"
+        role="alert"
+      >
+        <strong className="font-bold">Oh nein, 2min für uns!</strong>
+        <span className="block sm:inline mt-2">
+          Irgendwas hat beim Laden der Seite nicht geklappt. Falls das Problem
+          weiterhin besteht, melde dich bei unter{" "}
+          <a
+            href="mailto:webmaster@shcbelpa.ch"
+            className="underline hover:text-red-900"
+          >
+            webmaster@shcbelpa.ch
+          </a>
+        </span>
+      </div>
+    </SingleColumnPage>
   );
 }
